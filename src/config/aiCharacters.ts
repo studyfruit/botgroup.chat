3dd55b3065b52096d70f197b02d4746585a6ee63
studyfruit <86583093+studyfruit@users.noreply.github.com>
2025-05-11 19:19:52 +0800

    Update aiCharacters.ts

--- src/config/aiCharacters.ts
+++ src/config/aiCharacters.ts
@@ -1,11 +1,11 @@
 // 首先定义模型配置
 export const modelConfigs = [
-  // {
-  //   model: "qwen-plus",
-  //   apiKey: "DASHSCOPE_API_KEY", // 这里存储环境变量的 key 名称
-  //   baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1"
-  // },
-  // {
+  {
+    model: "qwen-plus",
+    apiKey: "DASHSCOPE_API_KEY", // 这里存储环境变量的 key 名称
+    baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1"
+  },
+  {
   //   model: "deepseek-v3-250324",
   //   apiKey: "ARK_API_KEY",
   //   baseURL: "https://ark.cn-beijing.volces.com/api/v3"
