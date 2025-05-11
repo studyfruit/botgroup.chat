23e9c6f88002983a81fa0eb2263ef39c7547b7c5
studyfruit <86583093+studyfruit@users.noreply.github.com>
2025-05-11 19:16:56 +0800

    Update aiCharacters.ts

--- src/config/aiCharacters.ts
+++ src/config/aiCharacters.ts
@@ -5,46 +5,46 @@ export const modelConfigs = [
   //   apiKey: "DASHSCOPE_API_KEY", // 这里存储环境变量的 key 名称
   //   baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1"
   // },
-  {
-    model: "deepseek-v3-250324",
-    apiKey: "ARK_API_KEY",
-    baseURL: "https://ark.cn-beijing.volces.com/api/v3"
-  },
   // {
-  //   model: "hunyuan-turbos-latest",
-  //   apiKey: "HUNYUAN_API_KEY1",
-  //   baseURL: "https://api.hunyuan.cloud.tencent.com/v1"
+  //   model: "deepseek-v3-250324",
+  //   apiKey: "ARK_API_KEY",
+  //   baseURL: "https://ark.cn-beijing.volces.com/api/v3"
   // },
+  {
+    model: "hunyuan-turbos-latest",
+    apiKey: "HUNYUAN_API_KEY1",
+    baseURL: "https://api.hunyuan.cloud.tencent.com/v1"
+  },
   {
     model: "bot-20250511011921-ttqnp",//豆包模型|火山引擎接入点（改成自己的）
     apiKey: "ARK_API_KEY",
     baseURL: "https://ark.cn-beijing.volces.com/api/v3/bots/chat/completions"
   },
-  // {
-  //   model: "ep-20250306223646-szzkw",//deepseekv火山引擎接入点（改成自己的）
-  //   apiKey: "ARK_API_KEY1",
-  //   baseURL: "https://ark.cn-beijing.volces.com/api/v3"
-  // },
-  // {
-  //   model: "glm-4-air",
-  //   apiKey: "GLM_API_KEY",
-  //   baseURL: "https://open.bigmodel.cn/api/paas/v4/"
-  // },
-  // {
-  //   model: "deepseek-chat",
-  //   apiKey: "DEEPSEEK_API_KEY",
-  //   baseURL: "https://api.deepseek.com/v1"
-  // },
+  {
+    model: "ep-20250306223646-szzkw",//deepseekv火山引擎接入点（改成自己的）
+    apiKey: "ARK_API_KEY1",
+    baseURL: "https://ark.cn-beijing.volces.com/api/v3"
+  },
+  {
+    model: "glm-4-air",
+    apiKey: "GLM_API_KEY",
+    baseURL: "https://open.bigmodel.cn/api/paas/v4/"
+  },
+  {
+    model: "deepseek-chat",
+    apiKey: "DEEPSEEK_API_KEY",
+    baseURL: "https://api.deepseek.com/v1"
+  },
   {
     model: "moonshot-v1-8k",
     apiKey: "KIMI_API_KEY",
     baseURL: "https://api.moonshot.cn/v1"
+  },
+  {
+    model: "ernie-3.5-128k",
+    apiKey: "BAIDU_API_KEY",
+    baseURL: "https://qianfan.baidubce.com/v2"
   }
-  // {
-  //   model: "ernie-3.5-128k",
-  //   apiKey: "BAIDU_API_KEY",
-  //   baseURL: "https://qianfan.baidubce.com/v2"
-  // }
 ] as const;
 export type ModelType = typeof modelConfigs[number]["model"];
 
